78b3bb776b29734ef95fcbc7cda9e33f72de2062
Hussain Pithawala <Hussain.Pithawala@synerzip.com>
2013-08-14 15:08:10 +0530

    removing unnecessary files

--- chapter_3/flights_3_2/src/main/java/org/packt/client/SearchRQFactory.java
+++
@@ -1,9 +0,0 @@
-package org.packt.client;
-
-import java.util.Date;
-
-import com.google.inject.assistedinject.Assisted;
-
-public interface SearchRQFactory {
-	SearchRQ create(@Assisted("depLoc") String depLoc,@Assisted("arrivLoc") String arrivLoc,Date flightDate);
-}
--- chapter_3/flights_3_2/src/main/java/org/packt/supplier/provider/XlCheckedProvider.java
+++
@@ -1,9 +0,0 @@
-package org.packt.supplier.provider;
-
-import org.packt.exceptions.NoXlAvailableException;
-
-import com.google.inject.throwingproviders.CheckedProvider;
-
-public interface XlCheckedProvider<T> extends CheckedProvider<T>{
-	T get() throws NoXlAvailableException;
-}
